74cfb85f743a7f6c03a985a329dffb2753a99821
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-11-13 15:09:31 -0300

    Preliminar version

--- fas_das_fotinhas/urls.py
+++ fas_das_fotinhas/urls.py
@@ -9,4 +9,5 @@ import tops.views
 urlpatterns = [
     url(r'^$', tops.views.index, name='index'),
     url(r'^auth$', tops.views.auth, name='auth'),
+    url(r'^auth_done$', tops.views.auth_done, name='auth_done'),
 ]
--- tops/views.py
+++ tops/views.py
@@ -3,7 +3,7 @@ from fas_das_fotinhas.settings import DEBUG, CLIENT_ID, CLIENT_SECRET
 
 import requests
 
-
+DEBUG = True
 if DEBUG:
     redirect_uri = '127.0.0.1:8080/auth_done'
 else:
@@ -14,7 +14,20 @@ def auth(request):
     url = 'https://api.instagram.com/oauth/authorize/?client_id={}&redirect_uri={}&response_type=code'.format(CLIENT_ID, redirect_uri)
     return render(request, 'auth.html', {'auth_url' : url})
 
+def auth_done(request):
+    code = request.GET.get('code')
+    r = requests.post('https://api.instagram.com/oauth/access_token',
+                      params={'client_id': CLIENT_ID,
+                              'client_secret': CLIENT_SECRET,
+                              'grant_type': 'authorization_code',
+                              'redirect_uri': redirect_uri,
+                              'code': code})
+    response = r.json()
+    access_token = response.get('access_token')
+    request.session['access_token'] = access_token
+    return redirect('index')
+
 def index(request):
     if 'access_token' not in request.session:
         return redirect('auth')
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'at': request.session.get('access_token')})
