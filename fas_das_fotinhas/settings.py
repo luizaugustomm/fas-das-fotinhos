e061f73c7493d4cbeb7d7bd7692fcec2fb839fe9
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-11-13 15:24:49 -0300

    Activated cookie-based sessions

--- fas_das_fotinhas/settings.py
+++ fas_das_fotinhas/settings.py
@@ -21,13 +21,15 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: change this before deploying to production!
-SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Cookie-based sessions
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 # Application definition
 
