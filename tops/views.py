63408455bfb86082e4fa05152b64d10eb98b46d8
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-11-13 15:42:51 -0300

    Created instagram client

--- tops/views.py
+++ tops/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from fas_das_fotinhas.settings import DEBUG, CLIENT_ID, CLIENT_SECRET
+from fas_das_fotinhas.client import Client
 
 import requests
+
 
 DEBUG = False
 if DEBUG:
@@ -10,24 +12,20 @@ else:
     redirect_uri = 'http://fas-das-fotinhas.herokuapp.com/auth_done'
 
 
+client = Client(CLIENT_ID, CLIENT_SECRET, redirect_uri)
+
+
 def auth(request):
-    url = 'https://api.instagram.com/oauth/authorize/?client_id={}&redirect_uri={}&response_type=code'.format(CLIENT_ID, redirect_uri)
-    return render(request, 'auth.html', {'auth_url' : url})
+    auth_url = client.get_auth_url()
+    return render(request, 'auth.html', {'auth_url' : auth_url})
 
 def auth_done(request):
     code = request.GET.get('code')
-    r = requests.post('https://api.instagram.com/oauth/access_token',
-                      params={'client_id': CLIENT_ID,
-                              'client_secret': CLIENT_SECRET,
-                              'grant_type': 'authorization_code',
-                              'redirect_uri': redirect_uri,
-                              'code': code})
-    response = r.json()
-    access_token = response.get('access_token')
+    access_token = client.exchange_for_token(code)
     request.session['access_token'] = access_token
     return redirect('index')
 
 def index(request):
     if 'access_token' not in request.session:
         return redirect('auth')
-    return render(request, 'index.html', {'at': request.session.get('access_token')})
+    return render(request, 'index.html')
